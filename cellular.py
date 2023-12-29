cc8963aa9f73ebce9b627123e39878f2b294739d
Rishi Gurjar <41022502+rishi-gurjar@users.noreply.github.com>
2023-12-29 09:11:00 +0900

    super sus

--- cellular.py
+++ cellular.py
@@ -12,6 +12,9 @@ class Cellular:
             self.lifeform.append(index)
     
     def __init__(self, chosen_col, rows=1, cols=5, epochs=5):
+        assert epochs > 2, "# of Epochs are too low"
+
+        self.deadforms = []
         self.lifeform = [chosen_col - 1]
         self.setArray(chosen_col, rows, cols)
         self.evolution(epochs)
@@ -21,33 +24,58 @@ class Cellular:
             print(row) 
             
     def dynamics(self):
-        lifeform_indexes = []
-        for life in self.lifeform:
-            count = 0
-            if life > 0 and self.array[0][life - 1] == True:
-                count+=1
-            if life > 1 and self.array[0][life - 2] == True:
-                count+=1
-            if life < len(self.array[0]) - 1 and self.array[0][life + 1] == True:
-                count+=1
-            if life < len(self.array[0]) - 2 and self.array[0][life + 2] == True:
-                count+=1
+        self.underpopulation()
+        self.overpopulation()
+        self.mating()
+    
+    def overpopulation(self):
+        index = 1
+        copy = self.array[0][:]
+        for col in copy[1:]:
+            if col == True and index <= len(self.array[0]) - 2:
+                #print(col, index)
+                if self.array[0][index - 1] == True and self.array[0][index + 1] == True:
+                    self.array[0][index] = False
+                    self.lifeform.remove(index)
+                    self.deadforms.append(index)
+            index+=1
 
-            if count < 2:
-                random_col = random.randint(1,len(self.array[0])) # SPONTANEOUS LIFE 
-                self.array[0][random_col - 1] = True
-                #self.array[0][life] == False # UNDERPOPULATION
-                lifeform_indexes.append(random_col - 1)
-                print('rand!')
-            elif count > 3:
-                self.array[0][life] == False # OVERPOPULATION
-                lifeform_indexes.remove(life)
+    def underpopulation(self):
+        index = 1
+        copy = self.array[0][:]
+        for col in copy[1:]:
+            if col == True and index <= len(self.array[0]) - 2:
+                #print(col, index)
+                if self.array[0][index - 1] == False and self.array[0][index + 1] == False:
+                    self.array[0][index] = False
+                    self.lifeform.remove(index)
+                    self.deadforms.append(index)
+            index+=1
+
+    def mating(self):
+        index = 0
+        for col in self.array[0]:
+            if col == False:
+                if index not in self.deadforms:
+                    self.deadforms.append(index)
+            index+=1
+        #print('Deadforms', self.deadforms)
+                    
+        for deadform in self.deadforms:
+            if deadform - 1 >= 0 and deadform < len(self.array[0]) - 1:
+                if deadform - 1 in self.lifeform and deadform + 1 in self.lifeform:
+                    self.array[0][deadform] = True
+
+                    self.updateLifeform(deadform)
+                    self.deadforms.remove(deadform)
             else:
-                self.array[0][life] == True # MATING
-                lifeform_indexes.append(life)
+                pass                
         
-        return lifeform_indexes
-        #self.reproduction()
+    
+    def spontaneous_reproduction(self): # SPONTANEOUS LIFE 
+        random_col = random.randint(1,len(self.array[0])) 
+        self.array[0][random_col - 1] = True
+        self.updateLifeform(random_col - 1)
 
     def reproduction(self): # REPRODUCES
         for col_index in range(1, len(self.array[0])): # iterating through columns
@@ -66,15 +94,17 @@ class Cellular:
                 return life - 1
 
     def evolution(self, epochs):
-        for _ in range(epochs):
+        self.showArray() # init array
+        #print("Lifeforms", self.lifeform)
+        for _ in range(2):
+            self.spontaneous_reproduction() # 2 spontaneous epochs
             self.showArray()
-            lifeform_indexes = self.dynamics()
-            
-            for index in lifeform_indexes:
-                self.updateLifeform(index)
-
-            #lifeform_index = self.binary_fission()
             #print("Lifeforms", self.lifeform)
 
+        for _ in range(epochs - 2): # (epochs - 2) epochs
+            self.dynamics()
+            self.showArray()
 
-cellular_instance = Cellular(3)
+            #print("Lifeforms", self.lifeform)
+
+cellular_instance = Cellular(chosen_col=5, cols=10, epochs=10)
